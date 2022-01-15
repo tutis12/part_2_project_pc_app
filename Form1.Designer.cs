92d682f1285a438b29fe68cba9156598a56e895b
tj322 <tj322@cam.ac.uk>
2022-01-15 17:24:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace capture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Black;
+            this.imageList1.Images.SetKeyName(0, "logo.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 738);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start camera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1298, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1298, 332);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "";
+            this.button2.Location = new System.Drawing.Point(1187, 738);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "done";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 831);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace capture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(camCollection[0].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+            mat = null;
+            grids = new SortedDictionary<long, bool[,]>();
+        }
+        private Bitmap Calc(Bitmap bm, int x0, int x1, int y0, int y1, int cx, int cy) {
+            cx *= 5;
+            cy *= 5;
+
+            BitmapData srcData = bm.LockBits(
+            new Rectangle(0, 0, bm.Width, bm.Height),
+            ImageLockMode.ReadOnly,
+            PixelFormat.Format32bppArgb);
+
+            int stride = srcData.Stride;
+
+            IntPtr Scan0 = srcData.Scan0;
+
+            long[,,] totals = new long[cx + 1, cy + 1, 3];
+
+            int width = bm.Width;
+            int height = bm.Height;
+            int szx = (x1 - x0) / cx;
+            int szy = (y1 - y0) / cy;
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+
+                for (int y = y0; y < y1; y++)
+                {
+                    for (int x = x0; x < x1; x++)
+                    {
+                        for (int color = 0; color < 3; color++)
+                        {
+                            int idx = (y * stride) + x * 4 + color;
+                            totals[(x - x0) / szx, (y - y0) / szy, color] += p[idx];
+                        }
+                    }
+                }
+            }
+            Bitmap ret = new Bitmap(cx, cy);
+            for (int i = 0; i < cx; i++)
+            {
+                for (int j = 0; j < cy; j++)
+                {
+                    long avgB = totals[i, j, 0] / (szx * szy);
+                    long avgG = totals[i, j, 1] / (szx * szy);
+                    long avgR = totals[i, j, 2] / (szx * szy);
+                    ret.SetPixel(i, j, Color.FromArgb((byte)avgR, (byte)avgG, (byte)avgB));
+                }
+            }
+            bm.UnlockBits(srcData);
+            return ret;
+        }
+        float ColDistance(Color a, Color b) {
+            return Math.Abs(a.GetHue() - b.GetHue());
+        }
+        Color Closest(double x)
+        {
+            if (x<255*0.5)
+                return Color.Black;
+            else
+                return Color.White;
+        }
+        private Bitmap Simplify(Bitmap bm)
+        {
+            Bitmap ret = new Bitmap(bm.Width/5, bm.Height/5);
+            for (int i = 0; i < bm.Width/5; i++) {
+                for (int j = 0; j < bm.Height/5; j++) {
+                    double sum = 0;
+                    double sumcnt = 0;
+                    for (int a = 1; a <= 3; a++)
+                        for (int b = 1; b <= 3; b++)
+                        {
+                            Color c = bm.GetPixel(i*5+a, j*5+b);
+                            int cnt = 3 - Math.Abs(a - 2) - Math.Abs(b - 2);
+                            sum += ((0.2125 * c.R) + (0.7154 * c.G) + (0.0721 * c.B)) * cnt;
+                            sumcnt += cnt;
+                        }
+                    sum /= sumcnt;
+                    ret.SetPixel(i, j, Closest(sum));
+                }
+            }
+            return ret;
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap b, int w, int h)
+        {
+            Bitmap ret = new Bitmap(w, h);
+            using (Graphics g = Graphics.FromImage(ret))
+            {
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(b, 0, 0, w, h);
+            }
+            return ret;
+        }
+        private bool[,] Convert(Bitmap x) {
+            bool[,] ret = new bool[x.Width, x.Height];
+            for (int i = 0; i < x.Width; i++) {
+                for (int j = 0; j < x.Height; j++) {
+                    Color c = x.GetPixel(i, j);
+                    if (c.R + c.G + c.B <= 128)
+                        ret[i, x.Height - j - 1] = true;
+                    else
+                        ret[i, x.Height - j - 1] = false;
+                }
+            }
+            return ret;
+        }
+        bool[,] mat = null;
+        bool same(bool[,] a, bool[,] b) {
+            if (a == null || b == null)
+                return false;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    if (a[i, j] != b[i, j])
+                        return false;
+            return true;
+        }
+        SortedDictionary<long, bool[,]> grids = new SortedDictionary<long, bool[,]>();
+        private void PrintMat(bool[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < a.GetLength(1); j++)
+                    if (a[i, j])
+                    {
+                        Console.Write('1');
+                    }
+                    else
+                    {
+                        Console.Write('0');
+                    }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        private void AddMat(bool[,] a)
+        {
+            long ms = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            grids.Add(ms, a);
+            Console.WriteLine(ms);
+        }
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap x = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap xx = (Bitmap)x.Clone();
+            int x0 = 160;
+            int x1 = 160 + 650;
+            int y0 = 35;
+            int y1 = 35 + 500;
+            Bitmap y = Calc(xx, x0, x1, y0, y1, 13, 10);
+            pictureBox2.Image = ResizeBitmap(y, 325, 250);
+            Bitmap z = Simplify(y);
+            pictureBox3.Image = ResizeBitmap(z, 325, 250);
+            bool[,] C = Convert(z);
+            if (same(C, mat) == false)
+            {
+                /*
+                for (int i = 0; i < C.GetLength(0); i++)
+                {
+                    for (int j = 0; j < C.GetLength(1); j++)
+                    {
+                        Console.Write(C[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();*/
+                AddMat(C);
+                PrintMat(C);
+                mat = C;
+            }
+            using (Graphics g = Graphics.FromImage(x))
+            {
+                Rectangle rect = new Rectangle(x0, y0, x1 - x0, y1 - y0);
+                g.DrawRectangle(new Pen(Color.Red, 7), rect);
+            }
+            pictureBox1.Image = x;
+        }
+
+        FilterInfoCollection camCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+            camCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+            {
+                videoCaptureDevice.Stop();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int Parity(bool[,] a) {
+            for (int t = 0; t < 2; t++)
+            {
+                int blog = 0;
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if ((i + j) % 2 == t)
+                        {
+                            if (a[i, j] == false)
+                                blog++;
+                        }
+                        else
+                        {
+                            if (a[i, j] == true)
+                                blog++;
+                        }
+                    }
+                }
+                if (blog < 22)
+                    return 1 - 2 * t;
+            }
+            return 0;
+        }
+        bool[,] GetAtTime(long t, List<long> ms, List<bool[,]> mat) {
+            int lo = 0;
+            int hi = ms.Count - 1;
+            while (lo < hi) {
+                int i = (lo + hi + 1) / 2;
+                if (ms[i] <= t)
+                    lo = i;
+                else
+                    hi = i - 1;
+            }
+            return mat[lo];
+        }
+        private int Parity1(bool[,] a)
+        {
+            int k0 = 0;
+            int k1 = 0;
+            for (int t = 0; t < 2; t++)
+            {
+                int blog = 0;
+                for (int i = 0; i < a.GetLength(0); i+= a.GetLength(0) - 1)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if ((i + j) % 2 == t)
+                        {
+                            if (a[i, j] == false)
+                                blog++;
+                        }
+                        else
+                        {
+                            if (a[i, j] == true)
+                                blog++;
+                        }
+                    }
+                }
+                if (t == 0)
+                    k0 = blog;
+                else
+                    k1 = blog;
+            }
+            if (k0 < k1)
+                return 0;
+            else
+                return 1;
+        }
+        long FixTime(long t, long dt, List<long> ms, List<bool[,]> mat)
+        {
+            int lo = 0;
+            int hi = ms.Count - 1;
+            while (lo < hi)
+            {
+                int i = (lo + hi + 1) / 2;
+                if (ms[i] <= t)
+                    lo = i;
+                else
+                    hi = i - 1;
+            }
+            hi = lo;
+            int x = Parity1(mat[lo]);
+            while (lo > 0 && Parity1(mat[lo - 1]) == x)
+                lo--;
+            while (hi < ms.Count - 1 && Parity1(mat[hi + 1]) == x)
+                hi++;
+            if (hi == ms.Count - 1)
+                return t;
+            long gal = (ms[lo] + ms[hi + 1]) / 2;
+            gal = Math.Max(gal, t - dt / 4);
+            gal = Math.Min(gal, t + dt / 4);
+            return gal;
+        }
+        bool[,] Rot180(bool[,] a) {
+            bool[,] b = new bool[a.GetLength(0), a.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    b[i, j] = a[a.GetLength(0) - i - 1, a.GetLength(1) - j - 1];
+                }
+            }
+            return b;
+        }
+        private void Calculate() {
+            List<long> ms = new List<long>();
+            List<bool[,]> mat = new List<bool[,]>();
+            List<int> par = new List<int>();
+            foreach (long k in grids.Keys) {
+                ms.Add(k);
+                par.Add(Parity(grids[k]));
+                mat.Add(grids[k]);
+            }
+            long mspl = -1;
+            long msmn = -1;
+            for (int i = 0; i < ms.Count; i++) {
+                if (par[i] == 1)
+                {
+                    if (i + 1 < ms.Count)
+                        mspl = ms[i + 1] - 1;
+                    else
+                        mspl = ms[i];
+                }
+                else if (par[i] == -1)
+                {
+                    if (i + 1 < ms.Count)
+                        msmn = ms[i + 1] - 1;
+                    else
+                        msmn = ms[i];
+                }
+            }
+            List<bool[,]> A = new List<bool[,]>();
+            long t0 = (mspl + msmn) / 2;
+            long dt = Math.Abs(msmn - mspl);
+            if (mspl == -1 || msmn == -1) {
+                Console.WriteLine("Try again!");
+            }
+            else
+            {
+                t0 += dt;
+                A.Add(GetAtTime(t0, ms, mat));
+                while (t0 + dt <= ms[ms.Count - 1])
+                {
+                    t0 += dt;
+                    t0 = FixTime(t0, dt, ms, mat);
+                    A.Add(GetAtTime(t0, ms, mat));
+                }
+            }
+            if (mspl > msmn)
+                for (int i = 0; i < A.Count; i++)
+                    A[i] = Rot180(A[i]);
+            List<bool[]> B = new List<bool[]>();
+            for (int i = 0; i < A.Count; i++)
+                B.Add(Flatten(A[i]));
+            List<bool> C = new List<bool>();
+            for (int i = 1; i < B.Count; i++)
+                foreach (bool b in B[i])
+                    C.Add(b);
+            int v = ToInt(B[0]);
+            v = v - (v % 8);
+            Console.WriteLine(v);
+            Console.WriteLine("YOOOO!!!");
+            foreach (bool[] i in B) {
+                foreach (bool a in i) {
+                    if (a)
+                        Console.Write("1");
+                    else
+                        Console.Write("0");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            while (C.Count > v)
+                C.RemoveAt(C.Count - 1);
+            WriteFile("file.txt", C);
+        }
+        private void WriteFile(string name, List<bool> C) {
+            List<byte> CC = new List<byte>();
+            for (int i = 0; i < C.Count; i += 8) {
+                int a = 0;
+                int x = 1;
+                for (int j = 0; j < 8 && i+j<C.Count; j++) 
+                {
+                    if (C[i + j] == true)
+                        a += x;
+                    x *= 2;
+                }
+                a -= 128;
+                if (a < 0)
+                    a += 256;
+                CC.Add((byte)a);
+            }
+            Console.WriteLine(CC.Count);
+            File.WriteAllBytes(name, CC.ToArray());
+        }
+        private int ToInt(bool[] a) {
+            int x = 1;
+            int sum = 0;
+            foreach (bool b in a) {
+                if (b == true) {
+                    sum += x;
+                }
+                x *= 2;
+            }
+            return sum;
+        }
+        private bool[] Flatten(bool [,] a)
+        {
+            bool[] b = new bool[(a.GetLength(0)-2) * a.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0)-2; i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    b[j + i * a.GetLength(1)] = a[i + 1, j];
+                }
+            }
+            return b;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+            {
+                videoCaptureDevice.Stop();
+            }
+            Calculate();
+        }
+    }
+}
